e802145dbc8771f1e33fac7d206afe61c95ed72e
batal.marklester@gmail.com <batal.marklester@gmail.com>
2022-04-25 03:59:26 +0800

    Added map in search results

--- src/main.js
+++ src/main.js
@@ -2,7 +2,19 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import './assets/css/style.css'
 import './assets/css/responsive.css'
-import './assets/js/custom.js'
 import $ from 'jquery'
+
+$(function () {
+
+        $(window).on('scroll', function() {
+            if($(window).scrollTop() > 10 ) {
+                $('.navbar').addClass('active');
+            } else {
+                $('.navbar').removeClass('active');
+            }
+        });
+
+});
+
 
 createApp(App).mount('#app')
