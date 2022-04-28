9998eb6540c4360f0d91cda885aa66271e02ecde
batal.marklester@gmail.com <batal.marklester@gmail.com>
2022-04-29 03:29:00 +0800

    Changed logo for the website

--- src/main.js
+++ src/main.js
@@ -2,6 +2,9 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import './assets/css/style.css'
 import './assets/css/responsive.css'
+
+import 'bootstrap'
+import 'popper.js'
 import $ from 'jquery'
 
 $(function () {
